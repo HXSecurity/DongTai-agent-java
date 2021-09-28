705fe0221c7aa984f4bc3f39ffd5195aa31beb43
‘niuerzhuang’ <niuerzhuang@huoxian.cn>
2021-09-28 13:45:03 +0800

    fix bug/issue-180

--- iast-agent/src/main/java/com/secnium/iast/agent/IastProperties.java
+++ iast-agent/src/main/java/com/secnium/iast/agent/IastProperties.java
@@ -120,9 +120,7 @@ public class IastProperties {
                 agentFile = new File(IastProperties.class.getProtectionDomain().getCodeSource().getLocation().getFile());
                 basePath = agentFile.getParentFile().getPath();
                 propertiesFilePath = basePath + File.separator + "config" + File.separator + "iast.properties";
-                if (propertiesFilePath.contains("%20")){
-                    propertiesFilePath = URLDecoder.decode(propertiesFilePath, "utf-8");
-                }
+                propertiesFilePath = URLDecoder.decode(propertiesFilePath, "utf-8");
             }
 
             propertiesFile = new File(propertiesFilePath);
