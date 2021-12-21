00cde0e5be9cc7fdbfcba567d4e9eb1667d529c9
Owefsad <16431735+exexute@users.noreply.github.com>
2021-12-21 11:52:36 +0800

    Fix: ignore git push

--- .github/workflows/version_update.sh
+++ .github/workflows/version_update.sh
@@ -2,7 +2,7 @@ OLD_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 NEW_VERSION=$1
 
 echo "curent path: `pwd`, change version $OLD_VERSION to $NEW_VERSION"
-
+exit 0
 sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" dongtai-jakarta-api/src/main/java/cn/huoxian/iast/api/ResponseWrapper.java
 sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" dongtai-servlet-api/src/main/java/cn/huoxian/iast/api/ResponseWrapper.java
 sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" iast-agent/src/main/java/com/secnium/iast/agent/Constant.java
