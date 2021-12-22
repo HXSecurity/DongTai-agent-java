b44dbc1b87d569b0cec11a994fd7df863a8784c3
owefsad <dongzhiyong@secnium.cn>
2021-12-22 15:58:07 +0800

    Fix: mvn deploy

--- .github/workflows/version_update.sh
+++ .github/workflows/version_update.sh
@@ -2,6 +2,11 @@ OLD_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 NEW_VERSION=$1
 
 echo "curent path: `pwd`, change version $OLD_VERSION to $NEW_VERSION"
+
+sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" dongtai-jakarta-api/src/main/java/cn/huoxian/iast/api/ResponseWrapper.java
+sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" dongtai-servlet-api/src/main/java/cn/huoxian/iast/api/ResponseWrapper.java
+sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" iast-agent/src/main/java/com/secnium/iast/agent/Constant.java
+
 # versions:set is a feature in JDK 1.7+ and Maven 3.3.1+
 mvn -q versions:set -DnewVersion="${NEW_VERSION}"
 mvn -q versions:update-child-modules
