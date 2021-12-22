d268b566a102f7ee83648f9a9c19f1fc417b47a1
Owefsad <16431735+exexute@users.noreply.github.com>
2021-12-22 16:17:21 +0800

    Fix: fix push

--- .github/workflows/version_update.sh
+++ .github/workflows/version_update.sh
@@ -2,7 +2,7 @@ OLD_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 NEW_VERSION=$1
 
 echo "curent path: `pwd`, change version $OLD_VERSION to $NEW_VERSION"
-
+exit 0
 sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" dongtai-jakarta-api/src/main/java/cn/huoxian/iast/api/ResponseWrapper.java
 sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" dongtai-servlet-api/src/main/java/cn/huoxian/iast/api/ResponseWrapper.java
 sed -i "s/v$OLD_VERSION/v$NEW_VERSION/g" iast-agent/src/main/java/com/secnium/iast/agent/Constant.java
@@ -13,9 +13,11 @@ mvn -q versions:set -DnewVersion="$NEW_VERSION"
 mvn -q versions:update-child-modules
 mvn -q versions:commit
 
-git config --global user.name 'exexute'
-git config --global user.email '1528360120@qq.com'
+git bran
+git config --global user.name '$GITHUB_ACTOR-bot'
+git config --global user.email '$GITHUB_ACTOR-bot@dongtai.io'
 git add .
 git commit -m "Update: change version from $OLD_VERSION to $NEW_VERSION"
 
-git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" "release-$NEW_VERSION"
+git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" refs/heads/release-$NEW_VERSION:refs/heads/release-$NEW_VERSION
+ 
