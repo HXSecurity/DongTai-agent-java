fc13b9a4b816b66257318f2a06629eea52e22af3
‘niuerzhuang’ <niuerzhuang@huoxian.cn>
2022-03-28 13:16:40 +0800

    change stats

--- dongtai-core/src/main/java/io/dongtai/iast/core/utils/threadlocal/IastTaintPool.java
+++ dongtai-core/src/main/java/io/dongtai/iast/core/utils/threadlocal/IastTaintPool.java
@@ -3,6 +3,8 @@ package io.dongtai.iast.core.utils.threadlocal;
 import io.dongtai.iast.core.EngineManager;
 import io.dongtai.iast.core.utils.PropertyUtils;
 import io.dongtai.iast.core.handler.hookpoint.models.MethodEvent;
+import io.dongtai.log.DongTaiLog;
+
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -29,55 +31,59 @@ public class IastTaintPool extends ThreadLocal<HashSet<Object>> {
      * @param obj source点的污点
      */
     public void addTaintToPool(Object obj, MethodEvent event, boolean isSource) {
-        int subHashCode = 0;
-        if (obj instanceof String[]) {
-            this.get().add(obj);
-            event.addTargetHash(obj.hashCode());
+        try{
+            int subHashCode = 0;
+            if (obj instanceof String[]) {
+                this.get().add(obj);
+                event.addTargetHash(obj.hashCode());
 
-            String[] tempObjs = (String[]) obj;
-            if (PROPERTIES.isNormalMode()) {
-                for (String tempObj : tempObjs) {
-                    this.get().add(tempObj);
-                    subHashCode = System.identityHashCode(tempObj);
+                String[] tempObjs = (String[]) obj;
+                if (PROPERTIES.isNormalMode()) {
+                    for (String tempObj : tempObjs) {
+                        this.get().add(tempObj);
+                        subHashCode = System.identityHashCode(tempObj);
+                        EngineManager.TAINT_HASH_CODES.get().add(subHashCode);
+                        event.addTargetHash(subHashCode);
+                    }
+                } else {
+                    for (String tempObj : tempObjs) {
+                        this.get().add(tempObj);
+                        event.addTargetHash(tempObj.hashCode());
+                    }
+                }
+            } else if (obj instanceof Map) {
+                this.get().add(obj);
+                event.addTargetHash(obj.hashCode());
+                if (isSource) {
+                    Map<String, String[]> tempMap = (Map<String, String[]>) obj;
+                    Set<Map.Entry<String, String[]>> entries = tempMap.entrySet();
+                    for (Map.Entry<String, String[]> entry : entries) {
+                        Object key = entry.getKey();
+                        Object value = entry.getValue();
+                        addTaintToPool(key, event, true);
+                        addTaintToPool(value, event, true);
+                    }
+                }
+            } else if (obj.getClass().isArray() && !obj.getClass().getComponentType().isPrimitive()) {
+                Object[] tempObjs = (Object[]) obj;
+                if (tempObjs.length != 0) {
+                    for (Object tempObj : tempObjs) {
+                        addTaintToPool(tempObj, event, isSource);
+                    }
+                }
+            } else {
+                this.get().add(obj);
+                if (obj instanceof String && PROPERTIES.isNormalMode()) {
+                    subHashCode = System.identityHashCode(obj);
                     EngineManager.TAINT_HASH_CODES.get().add(subHashCode);
-                    event.addTargetHash(subHashCode);
+                } else {
+                    subHashCode = obj.hashCode();
                 }
-            } else {
-                for (String tempObj : tempObjs) {
-                    this.get().add(tempObj);
-                    event.addTargetHash(tempObj.hashCode());
-                }
-            }
-        } else if (obj instanceof Map) {
-            this.get().add(obj);
-            event.addTargetHash(obj.hashCode());
-            if (isSource) {
-                Map<String, String[]> tempMap = (Map<String, String[]>) obj;
-                Set<Map.Entry<String, String[]>> entries = tempMap.entrySet();
-                for (Map.Entry<String, String[]> entry : entries) {
-                    Object key = entry.getKey();
-                    Object value = entry.getValue();
-                    addTaintToPool(key, event, true);
-                    addTaintToPool(value, event, true);
-                }
-            }
-        } else if (obj.getClass().isArray() && !obj.getClass().getComponentType().isPrimitive()) {
-            Object[] tempObjs = (Object[]) obj;
-            if (tempObjs.length != 0) {
-                for (Object tempObj : tempObjs) {
-                    addTaintToPool(tempObj, event, isSource);
-                }
-            }
-        } else {
-            this.get().add(obj);
-            if (obj instanceof String && PROPERTIES.isNormalMode()) {
-                subHashCode = System.identityHashCode(obj);
-                EngineManager.TAINT_HASH_CODES.get().add(subHashCode);
-            } else {
-                subHashCode = obj.hashCode();
-            }
-            event.addTargetHash(subHashCode);
+                event.addTargetHash(subHashCode);
 
+            }
+        }catch (Exception e){
+            DongTaiLog.debug(e);
         }
     }
 
