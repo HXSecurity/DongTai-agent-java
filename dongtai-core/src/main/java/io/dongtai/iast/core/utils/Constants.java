3318d9d9652cd49189459e4c9aabce51a3689c8e
‘niuerzhuang’ <niuerzhuang@huoxian.cn>
2022-04-07 18:44:16 +0800

    close issue #282

--- dongtai-core/src/main/java/io/dongtai/iast/core/utils/Constants.java
+++ dongtai-core/src/main/java/io/dongtai/iast/core/utils/Constants.java
@@ -5,7 +5,6 @@ package io.dongtai.iast.core.utils;
  */
 public class Constants {
     public static final String AGENT_VERSION_VALUE = "v1.4.0";
-    public final static String API_AGENT_REGISTER = "/api/v1/agent/register";
     public final static String API_REPORT_UPLOAD = "/api/v1/report/upload";
     public final static String SERVER_ADDRESS = "/api/v1/agent/update";
     public final static String API_HOOK_PROFILE = "/api/v1/profiles";
