d7fd178be60991056b579ed430101a7029e6b5f5
‘niuerzhuang’ <niuerzhuang@huoxian.cn>
2022-06-24 16:50:09 +0800

    Release 1.7.2

--- dongtai-api/src/main/java/io/dongtai/api/jakarta/JakartaResponseWrapper.java
+++ dongtai-api/src/main/java/io/dongtai/api/jakarta/JakartaResponseWrapper.java
@@ -23,7 +23,7 @@ public class JakartaResponseWrapper extends HttpServletResponseWrapper implement
 
     public JakartaResponseWrapper(HttpServletResponse response) {
         super(response);
-        response.addHeader("DongTai", "v1.7.1");
+        response.addHeader("DongTai", "v1.7.2");
     }
 
     @Override
