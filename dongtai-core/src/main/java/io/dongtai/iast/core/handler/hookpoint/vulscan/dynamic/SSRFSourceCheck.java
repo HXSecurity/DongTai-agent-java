13412caab3173653d27ed5d9b9a87b3117670082
lostsnow <lostsnow@gmail.com>
2022-09-01 18:09:44 +0800

    fixes apache httpclient5 taint source

--- dongtai-core/src/main/java/io/dongtai/iast/core/handler/hookpoint/vulscan/dynamic/SSRFSourceCheck.java
+++ dongtai-core/src/main/java/io/dongtai/iast/core/handler/hookpoint/vulscan/dynamic/SSRFSourceCheck.java
@@ -18,12 +18,14 @@ public class SSRFSourceCheck {
     // TODO: use method execute for sink
     private static final String APACHE_HTTP_CLIENT_REQUEST_SET_URI = " org.apache.http.client.methods.HttpRequestBase.setURI(java.net.URI)".substring(1);
     private static final String APACHE_LEGACY_HTTP_CLIENT_REQUEST_SET_URI = " org.apache.commons.httpclient.HttpMethodBase.setURI(org.apache.commons.httpclient.URI)".substring(1);
+    private static final String APACHE_HTTP_CLIENT5_EXECUTE = " org.apache.hc.client5.http.impl.classic.CloseableHttpClient.doExecute(org.apache.hc.core5.http.HttpHost,org.apache.hc.core5.http.ClassicHttpRequest,org.apache.hc.core5.http.protocol.HttpContext)".substring(1);
     private static final String OKHTTP3_CALL_EXECUTE = "okhttp3.Call.execute()";
     private static final String OKHTTP3_CALL_ENQUEUE = "okhttp3.Call.enqueue(okhttp3.Callback)";
     private static final String OKHTTP_CALL_EXECUTE = "com.squareup.okhttp.Call.execute()";
     private static final String OKHTTP_CALL_ENQUEUE = "com.squareup.okhttp.Call.enqueue(com.squareup.okhttp.Callback)";
 
     private static final String APACHE_LEGACY_HTTP_CLIENT_URI = " org.apache.commons.httpclient.URI".substring(1);
+    private static final String APACHE_HTTP_CLIENT5_HTTP_REQUEST = " org.apache.hc.client5.http.classic.methods.HttpUriRequestBase".substring(1);
     private static final String OKHTTP3_INTERNAL_REAL_CALL = "okhttp3.internal.connection.RealCall";
     private static final String OKHTTP3_REAL_CALL = "okhttp3.RealCall";
     private static final String OKHTTP_CALL = "com.squareup.okhttp.Call";
@@ -34,6 +36,7 @@ public class SSRFSourceCheck {
             JAVA_NET_URL_OPEN_STREAM,
             APACHE_HTTP_CLIENT_REQUEST_SET_URI,
             APACHE_LEGACY_HTTP_CLIENT_REQUEST_SET_URI,
+            APACHE_HTTP_CLIENT5_EXECUTE,
             OKHTTP3_CALL_EXECUTE,
             OKHTTP3_CALL_ENQUEUE,
             OKHTTP_CALL_EXECUTE,
@@ -52,7 +55,9 @@ public class SSRFSourceCheck {
             return javaNetURLSourceHit(event, sink);
         } else if (APACHE_HTTP_CLIENT_REQUEST_SET_URI.equals(sink.getSignature())
                 || APACHE_LEGACY_HTTP_CLIENT_REQUEST_SET_URI.equals(sink.getSignature())) {
-            return apacheHttpClientRequestSetURISourceHit(event, sink);
+            return apacheHttpClientSourceHit(event, sink);
+        } else if (APACHE_HTTP_CLIENT5_EXECUTE.equals(sink.getSignature())) {
+            return apacheHttpClient5SourceHit(event, sink);
         } else if (OKHTTP3_CALL_EXECUTE.equals(sink.getSignature())
                 || OKHTTP3_CALL_ENQUEUE.equals(sink.getSignature())
                 || OKHTTP_CALL_EXECUTE.equals(sink.getSignature())
@@ -69,14 +74,16 @@ public class SSRFSourceCheck {
             }
 
             URL url = (URL) u;
-            String protocol = url.getProtocol();
-            String userInfo = url.getUserInfo();
-            String host = url.getHost();
-            String path = url.getPath();
-            String query = url.getQuery();
+            Map<String, Object> sourceMap = new HashMap<String, Object>() {{
+                put("PROTOCOL", url.getProtocol());
+                put("USERINFO", url.getUserInfo());
+                put("HOST", url.getHost());
+                put("PATH", url.getPath());
+                put("QUERY", url.getQuery());
+            }};
 
             event.setInValue(url.toString());
-            return addSourceType(event, protocol, userInfo, host, path, query);
+            return addSourceType(event, sourceMap);
         } catch (Exception e) {
             DongTaiLog.warn("java.net.URL get source failed: " + e.getMessage());
             return false;
@@ -90,14 +97,16 @@ public class SSRFSourceCheck {
             }
 
             URI uri = (URI) u;
-            String protocol = uri.getScheme();
-            String userInfo = uri.getUserInfo();
-            String host = uri.getHost();
-            String path = uri.getPath();
-            String query = uri.getQuery();
+            Map<String, Object> sourceMap = new HashMap<String, Object>() {{
+                put("PROTOCOL", uri.getScheme());
+                put("USERINFO", uri.getUserInfo());
+                put("HOST", uri.getHost());
+                put("PATH", uri.getPath());
+                put("QUERY", uri.getQuery());
+            }};
 
             event.setInValue(uri.toString());
-            return addSourceType(event, protocol, userInfo, host, path, query);
+            return addSourceType(event, sourceMap);
         } catch (Exception e) {
             DongTaiLog.warn("java.net.URI get source failed: " + e.getMessage());
             return false;
@@ -108,7 +117,7 @@ public class SSRFSourceCheck {
         return processJavaNetUrl(event, event.object);
     }
 
-    private static boolean apacheHttpClientRequestSetURISourceHit(MethodEvent event, IastSinkModel sink) {
+    private static boolean apacheHttpClientSourceHit(MethodEvent event, IastSinkModel sink) {
         try {
             if (event.argumentArray.length < 1 || event.argumentArray[0] == null) {
                 return false;
@@ -118,19 +127,51 @@ public class SSRFSourceCheck {
             if (obj instanceof URI) {
                 return processJavaNetUri(event, obj);
             } else if (APACHE_LEGACY_HTTP_CLIENT_URI.equals(obj.getClass().getName())) {
-                Object protocol = obj.getClass().getMethod("getRawScheme").invoke(obj);
-                Object userInfo = obj.getClass().getMethod("getRawUserinfo").invoke(obj);
-                Object host = obj.getClass().getMethod("getRawHost").invoke(obj);
-                Object path = obj.getClass().getMethod("getRawPath").invoke(obj);
-                Object query = obj.getClass().getMethod("getRawQuery").invoke(obj);
+                Map<String, Object> sourceMap = new HashMap<String, Object>() {{
+                    put("PROTOCOL", obj.getClass().getMethod("getRawScheme").invoke(obj));
+                    put("USERINFO", obj.getClass().getMethod("getRawUserinfo").invoke(obj));
+                    put("HOST", obj.getClass().getMethod("getRawHost").invoke(obj));
+                    put("PATH", obj.getClass().getMethod("getRawPath").invoke(obj));
+                    put("QUERY", obj.getClass().getMethod("getRawQuery").invoke(obj));
+                }};
 
                 event.setInValue((String) obj.getClass().getMethod("toString").invoke(obj));
-                return addSourceType(event, protocol, userInfo, host, path, query);
+                return addSourceType(event, sourceMap);
             }
 
             return false;
         } catch (Exception e) {
             DongTaiLog.warn("apache http client get source failed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    private static boolean apacheHttpClient5SourceHit(MethodEvent event, IastSinkModel sink) {
+        try {
+            if (event.argumentArray.length < 2 || event.argumentArray[1] == null) {
+                return false;
+            }
+
+            Object reqObj = event.argumentArray[1];
+            if (APACHE_HTTP_CLIENT5_HTTP_REQUEST.equals(reqObj.getClass().getName())
+                    || APACHE_HTTP_CLIENT5_HTTP_REQUEST.equals(reqObj.getClass().getSuperclass().getName())) {
+                Object authorityObj = reqObj.getClass().getMethod("getAuthority").invoke(reqObj);
+                Map<String, Object> sourceMap = new HashMap<String, Object>() {{
+                    put("PROTOCOL", reqObj.getClass().getMethod("getScheme").invoke(reqObj));
+                    put("USERINFO", authorityObj.getClass().getMethod("getUserInfo").invoke(authorityObj));
+                    put("HOST", authorityObj.getClass().getMethod("getHostName").invoke(authorityObj));
+                    // getPath = path + query
+                    put("PATHQUERY", reqObj.getClass().getMethod("getPath").invoke(reqObj));
+                }};
+
+                Object uriObj = reqObj.getClass().getMethod("getUri").invoke(reqObj);
+                event.setInValue((String) uriObj.getClass().getMethod("toString").invoke(uriObj));
+                return addSourceType(event, sourceMap);
+            }
+
+            return false;
+        } catch (Exception e) {
+            DongTaiLog.warn("apache http client5 get source failed: " + e.getMessage());
             return false;
         }
     }
@@ -158,14 +199,17 @@ public class SSRFSourceCheck {
                     return false;
                 }
 
-                String protocol = (String) url.getClass().getMethod("scheme").invoke(url);
-                String userInfo = (String) url.getClass().getMethod("username").invoke(url);
-                String host = (String) url.getClass().getMethod("host").invoke(url);
-                String path = (String) url.getClass().getMethod("encodedPath").invoke(url);
-                String query = (String) url.getClass().getMethod("query").invoke(url);
+                Map<String, Object> sourceMap = new HashMap<String, Object>() {{
+                    put("PROTOCOL", url.getClass().getMethod("scheme").invoke(url));
+                    put("USERNAME", url.getClass().getMethod("username").invoke(url));
+                    put("PASSWORD", url.getClass().getMethod("password").invoke(url));
+                    put("HOST", url.getClass().getMethod("host").invoke(url));
+                    put("PATH", url.getClass().getMethod("encodedPath").invoke(url));
+                    put("QUERY", url.getClass().getMethod("query").invoke(url));
+                }};
 
                 event.setInValue((String) url.getClass().getMethod("toString").invoke(url));
-                return addSourceType(event, protocol, userInfo, host, path, query);
+                return addSourceType(event, sourceMap);
             }
 
             return false;
@@ -175,32 +219,20 @@ public class SSRFSourceCheck {
         }
     }
 
-    private static boolean addSourceType(MethodEvent event, Object protocol, Object userInfo, Object host, Object path, Object query) {
-        boolean hit1 = !"".equals(protocol) && TaintPoolUtils.poolContains(protocol, event);
-        boolean hit2 = !"".equals(userInfo) && TaintPoolUtils.poolContains(userInfo, event);
-        boolean hit3 = !"".equals(host) && TaintPoolUtils.poolContains(host, event);
-        boolean hit4 = !"".equals(path) && TaintPoolUtils.poolContains(path, event);
-        boolean hit5 = !"".equals(query) && TaintPoolUtils.poolContains(query, event);
-        event.inValue = event.object.toString();
-        boolean hit = hit1 || hit2 || hit3 || hit4 || hit5;
-        if (hit && event.sourceTypes == null) {
-            event.sourceTypes = new ArrayList<MethodEvent.MethodEventSourceType>();
-            if (hit1) {
-                event.sourceTypes.add(new MethodEvent.MethodEventSourceType(System.identityHashCode(protocol), "PROTOCOL"));
-            }
-            if (hit2) {
-                event.sourceTypes.add(new MethodEvent.MethodEventSourceType(System.identityHashCode(userInfo), "USERINFO"));
-            }
-            if (hit3) {
-                event.sourceTypes.add(new MethodEvent.MethodEventSourceType(System.identityHashCode(host), "HOST"));
-            }
-            if (hit4) {
-                event.sourceTypes.add(new MethodEvent.MethodEventSourceType(System.identityHashCode(path), "PATH"));
-            }
-            if (hit5) {
-                event.sourceTypes.add(new MethodEvent.MethodEventSourceType(System.identityHashCode(query), "QUERY"));
+    private static boolean addSourceType(MethodEvent event, Map<String, Object> sourceMap) {
+        boolean hit = false;
+        event.sourceTypes = new ArrayList<MethodEvent.MethodEventSourceType>();
+        for (Map.Entry<String, Object> entry : sourceMap.entrySet()) {
+            if (!"".equals(entry.getValue()) && TaintPoolUtils.poolContains(entry.getValue(), event)) {
+                event.sourceTypes.add(new MethodEvent.MethodEventSourceType(System.identityHashCode(entry.getValue()), entry.getKey()));
+                hit = true;
             }
         }
+
+        if (event.sourceTypes.size() == 0) {
+            event.sourceTypes = null;
+        }
+
         return hit;
     }
 }
