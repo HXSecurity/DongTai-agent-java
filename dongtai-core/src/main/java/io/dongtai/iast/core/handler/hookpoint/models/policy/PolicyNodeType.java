4ebb1ea5971fe0e998c0c66e51bca49b2787464a
lostsnow <lostsnow@gmail.com>
2022-10-21 17:42:55 +0800

    add policy node type

---
+++ dongtai-core/src/main/java/io/dongtai/iast/core/handler/hookpoint/models/policy/PolicyNodeType.java
@@ -0,0 +1,38 @@
+package io.dongtai.iast.core.handler.hookpoint.models.policy;
+
+public enum PolicyNodeType {
+    SOURCE(2, "source"),
+    PROPAGATOR(1, "propagator"),
+    FILTER(3, "filter"),
+    SINK(4, "sink"),
+    ;
+
+    private final int type;
+    private final String name;
+
+    PolicyNodeType(int type, String name) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static PolicyNodeType get(Integer type) {
+        if (type == null) {
+            return null;
+        }
+        PolicyNodeType[] values = values();
+        for (PolicyNodeType policyNodeType : values) {
+            if (policyNodeType.type == type) {
+                return policyNodeType;
+            }
+        }
+        return null;
+    }
+}
---
+++ dongtai-core/src/test/java/io/dongtai/iast/core/handler/hookpoint/models/policy/PolicyNodeTypeTest.java
@@ -0,0 +1,26 @@
+package io.dongtai.iast.core.handler.hookpoint.models.policy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PolicyNodeTypeTest {
+    @Test
+    public void testGet() {
+        Map<Integer, PolicyNodeType> tests = new HashMap<Integer, PolicyNodeType>(){{
+            put(null, null);
+            put(0, null);
+            put(1, PolicyNodeType.PROPAGATOR);
+            put(2, PolicyNodeType.SOURCE);
+            put(3, PolicyNodeType.FILTER);
+            put(4, PolicyNodeType.SINK);
+            put(5, null);
+        }};
+
+        for (Map.Entry<Integer, PolicyNodeType> entry : tests.entrySet()) {
+            Assert.assertEquals("get " + entry.getKey(), entry.getValue(), PolicyNodeType.get(entry.getKey()));
+        }
+    }
+}
