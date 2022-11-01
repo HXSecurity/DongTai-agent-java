28142bffbd5f5ce01d39dff72af1cdcf9e6c455f
lostsnow <lostsnow@gmail.com>
2022-11-01 18:40:39 +0800

    clean unused plugins

--- dongtai-core/src/main/java/io/dongtai/iast/core/bytecode/enhance/plugin/PluginRegister.java
+++ dongtai-core/src/main/java/io/dongtai/iast/core/bytecode/enhance/plugin/PluginRegister.java
@@ -27,15 +27,11 @@ public class PluginRegister {
         this.plugins = new ArrayList<DispatchPlugin>();
         this.plugins.add(new DispatchSpringApplication());
         this.plugins.add(new DispatchJ2ee());
-        //PLUGINS.add(new DispatchJsp());
-        // this.plugins.add(new DispatchCookie());
         // this.plugins.add(new DispatchDubbo());
         // this.plugins.add(new DispatchKafka());
         // this.plugins.add(new DispatchJdbc());
         this.plugins.add(new DispatchShiro());
-//        this.plugins.add(new DispatchHandlerInterceptor());
 
-        //PLUGINS.add(new DispatchSpringAutoBinding());
         this.plugins.add(new DispatchClassPlugin());
         // plugins.add(new DispatchGrpc());
     }
