f17105c625712700a29aecd78ff3cd16af1468d1
lostsnow <lostsnow@gmail.com>
2022-11-11 16:56:22 +0800

    bump version to 1.8.0

--- dongtai-common/src/main/java/io/dongtai/iast/common/constants/AgentConstant.java
+++ dongtai-common/src/main/java/io/dongtai/iast/common/constants/AgentConstant.java
@@ -1,7 +1,7 @@
 package io.dongtai.iast.common.constants;
 
 public class AgentConstant {
-    public static final String VERSION_VALUE = "v1.7.9";
+    public static final String VERSION_VALUE = "v1.8.0";
     public static final String LANGUAGE = "JAVA";
     public static final String THREAD_NAME_PREFIX = "DongTai-IAST-";
 }
