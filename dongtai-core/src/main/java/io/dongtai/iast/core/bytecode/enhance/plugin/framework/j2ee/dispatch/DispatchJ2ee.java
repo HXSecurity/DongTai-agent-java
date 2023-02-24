bac0fd47f7d2681e7d94267cf701ba120f04fc7a
lostsnow <lostsnow@gmail.com>
2023-02-24 17:57:42 +0800

    add undertow servlet ServletPrintWriter response fetch

--- dongtai-core/src/main/java/io/dongtai/iast/core/bytecode/enhance/plugin/framework/j2ee/dispatch/DispatchJ2ee.java
+++ dongtai-core/src/main/java/io/dongtai/iast/core/bytecode/enhance/plugin/framework/j2ee/dispatch/DispatchJ2ee.java
@@ -23,6 +23,7 @@ public class DispatchJ2ee implements DispatchPlugin {
     public static final String JAVAX_SERVLET_OUTPUT_STREAM = " javax.servlet.ServletOutputStream".substring(1);
     public static final String JAKARTA_SERVLET_OUTPUT_STREAM = " jakarta.servlet.ServletOutputStream".substring(1);
     public static final String APACHE_COYOTE_WRITER = " org.apache.catalina.connector.CoyoteWriter".substring(1);
+    public static final String UNDERTOW_SERVLET_WRITER = " io.undertow.servlet.spec.ServletPrintWriter".substring(1);
 
     @Override
     public ClassVisitor dispatch(ClassVisitor classVisitor, ClassContext context, Policy policy) {
@@ -41,7 +42,7 @@ public class DispatchJ2ee implements DispatchPlugin {
             classVisitor = new ServletOutputStreamAdapter(classVisitor, context);
         } else if (ancestors.contains(JAKARTA_SERVLET_OUTPUT_STREAM)) {
             classVisitor = new ServletOutputStreamAdapter(classVisitor, context);
-        } else if (APACHE_COYOTE_WRITER.equals(className)) {
+        } else if (APACHE_COYOTE_WRITER.equals(className) || UNDERTOW_SERVLET_WRITER.equals(className)) {
             classVisitor = new PrintWriterAdapter(classVisitor, context);
         }
         return classVisitor;
